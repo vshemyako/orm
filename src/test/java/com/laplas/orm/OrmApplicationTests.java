612c41194cc38e4033760e13e068acee9c90833f
Valentine Shemyako <valentineshemyako@rentalcars.com>
2020-01-20 22:00:23 +0300

    orm: Initialized base project structure

---
+++ src/test/java/com/laplas/orm/OrmApplicationTests.java
@@ -0,0 +1,12 @@
+package com.laplas.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
