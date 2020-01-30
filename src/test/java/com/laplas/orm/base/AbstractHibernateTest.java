fbd9290245d92428df84a501d5a8194c9eff2628
Valentine Shemyako <valentineshemyako@rentalcars.com>
2020-01-30 19:53:07 +0300

    orm: Added method to execute database access logic using entity-manager

--- src/test/java/com/laplas/orm/base/AbstractHibernateTest.java
+++ src/test/java/com/laplas/orm/base/AbstractHibernateTest.java
@@ -2,25 +2,58 @@ package com.laplas.orm.base;
 
 import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
 import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.spi.PersistenceUnitInfo;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public abstract class AbstractHibernateTest {
 
     private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
 
-    @Test
+    @BeforeEach
     void setUp() {
         entityManagerFactory = createEntityManagerFactory();
+        entityManager = createEntityManager();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (entityManager != null) {
+            entityManager.close();
+        }
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
     }
 
     protected abstract Class<?>[] getEntitiesClasses();
+
+    protected <T> T execute(Function<EntityManager, T> function) {
+        T result;
+        EntityTransaction txn = null;
+        try {
+            txn = entityManager.getTransaction();
+            txn.begin();
+            result = function.apply(entityManager);
+            txn.commit();
+        } catch (Throwable t) {
+            if (txn != null && txn.isActive()) {
+                txn.rollback();
+            }
+            throw t;
+        }
+        return result;
+    }
 
     private List<String> getEntitiesClassesNames() {
         return Arrays.stream(getEntitiesClasses())
@@ -43,5 +76,9 @@ public abstract class AbstractHibernateTest {
         unitInfo.setPersistenceUnitName(getUnitInfoName());
         unitInfo.addMappingFileNames(getEntitiesClassesNames());
         return unitInfo;
+    }
+
+    private EntityManager createEntityManager() {
+        return this.entityManagerFactory.createEntityManager();
     }
 }
